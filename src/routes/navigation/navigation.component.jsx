c8e485274bc20a55eeb8bff10803f2e4782e38ac
Tara Jaikaeo <tjaikaeo@gmail.com>
2025-05-08 16:42:13 -0400

    Add navigation component and logo

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,22 @@
+import { Fragment } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import { ReactComponent as ShLogo } from '../../assets/preloved.svg';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+        <div className="navigation">
+            <Link className="logo-container" to='/'>
+                <ShLogo className="logo" />
+            </Link>
+            <div className="nav-links-container">
+                <Link className="nav-link" to='/Shop'>Shop</Link>
+            </div>
+        </div>
+        <Outlet />
+    </Fragment>
+  );
+}
+
+export default Navigation;
