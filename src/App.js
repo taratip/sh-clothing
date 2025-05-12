b8ad7fca4b3ad0154309b42f2e0f89f5e54840ef
Tara Jaikaeo <tjaikaeo@gmail.com>
2025-05-12 17:20:36 -0400

    Add firebase and googleAuth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   )
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -11,7 +11,10 @@ const Navigation = () => {
                 <ShLogo className="logo" />
             </Link>
             <div className="nav-links-container">
-                <Link className="nav-link" to='/Shop'>Shop</Link>
+                <Link className="nav-link" to='/shop'>SHOP</Link>
+            </div>
+            <div className="nav-links-container">
+                <Link className="nav-link" to='/sign-in'>SIGN IN</Link>
             </div>
         </div>
         <Outlet />
